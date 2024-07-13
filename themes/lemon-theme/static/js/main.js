12fb7a4b92d18958f85c4d9db3794ed6912e2c29
Laura Sinclair <l.sinclair@tuta.io>
2024-07-13 02:00:33 +0200

    Remove unused javascript files

--- themes/lemon-theme/static/js/main.js
+++ themes/lemon-theme/static/js/main.js
@@ -1,50 +1,24 @@
-// ScrollReveal().reveal('.sr-down', {
-// 	distance: '5%',
-// 	origin: 'top',
-// 	// opacity: 0,
-// 	easing: 'ease-in-out',
-// 	duration: 500,
-// 	interval: 100	
-// });
-
-$(document).ready(function() {
-  $(window).scroll(function() {
-	if ($(document).scrollTop() > 250) {
-	  $("#logo").addClass("reveal");
-	} else {
-	  $("#logo").removeClass("reveal");
-	}
-  });
+$(document).ready(function () {
+	$(window).scroll(function () {
+		if ($(document).scrollTop() > 250) {
+			$('#logo').addClass('reveal');
+		} else {
+			$('#logo').removeClass('reveal');
+		}
+	});
 });
 
-// $(document).ready(function() {
-//   $(window).scroll(function() {
-// 	if ($(document).scrollTop() > 100) {
-// 	  $(".icon-lemon").addClass("reveal");
-// 	} else {
-// 	  $(".icon-lemon").removeClass("reveal");
-// 	}
-//   });
-// });
-
-// $(document).ready(function() {
-//   $(window).scroll(function() {
-// 	if ($(document).scrollTop() > 1) {
-// 	  $(".nav-element").addClass("hide");
-// 	} else {
-// 	  $(".nav-element").removeClass("hide");
-// 	}
-//   });
-// });
-
-$(document).ready(function() {
- $(window).scroll(function(){
-        if($(window).scrollTop() + $(window).height() > $(document).height() - ($("footer").height()) + 550) {
-            $('.nav-element').addClass("hide");
-        } else {
-            $('.nav-element').removeClass("hide");
+$(document).ready(function () {
+	$(window).scroll(function () {
+		if (
+			$(window).scrollTop() + $(window).height() >
+			$(document).height() - $('footer').height() + 550
+		) {
+			$('.nav-element').addClass('hide');
+		} else {
+			$('.nav-element').removeClass('hide');
 		}
-    });
+	});
 });
 
 var srDown = {
@@ -52,18 +26,15 @@ var srDown = {
 	origin: 'top',
 	easing: 'ease-in-out',
 	duration: 500,
-	interval: 150
+	interval: 150,
 };
 
 var srFade = {
 	distance: '0',
 	// opacity: null,
 	duration: 500,
-	interval: 150
+	interval: 150,
 };
 
 var nodeList = document.querySelectorAll('.sr-down');
 var otherNodeList = document.querySelectorAll('.work-section img');
-
-// ScrollReveal().reveal(nodeList, srDown);
-// ScrollReveal().reveal(otherNodeList, srFade);
